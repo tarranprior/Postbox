3b2be3393ea598988454988e3bbceb4c41cc8945
Tarran Prior <Tarranprior@protonmail.com>
2025-02-17 01:22:48 +0000

    📫 feat: add System.CommandLine, update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+namespace Postbox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var root = new RootCommand("📫 Postbox is a lightweight command-line RSA encryption tool which allows users to generate key pairs, exchange public keys, encrypt and decrypt messages, and communicate securely over SMTP.");
+
+        var messageOption = new Option<string>(["-m", "--message"], "Specify a message.");
+        var fileOption = new Option<string>(["-f", "--file"], "Specify a file.");
+        var keyOption = new Option<string>(["-k", "--key"], "Specify a public/private key.");
+        var outputOption = new Option<string>(["-o", "--output"], "Specify an output file.");
+        var emailOption = new Option<string>(["-e", "--email"], "Specify an email address.");
+
+        root.Invoke(args);
+    }
+}
