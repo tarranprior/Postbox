6533445306a0ebd44bcb7078d05cc45883d3fa80
Tarran Prior <Tarranprior@protonmail.com>
2025-02-17 04:43:51 +0000

    📫 docs: add XML to `KeyManager.cs`

--- src/KeyManagement/KeyManager.cs
+++ src/KeyManagement/KeyManager.cs
@@ -3,11 +3,25 @@ using Serilog;
 
 namespace Postbox.KeyManagement;
 
+/// <summary>
+/// Provides functionality for key management.
+/// </summary>
 public static class KeyManager
 {
+    /// <summary>
+    /// The default directory where key files are written.
+    /// </summary>
     private static readonly string DefaultDirectory =
     Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Postbox", "Keys");
 
+    /// <summary>
+    /// Generates an RSA key pair and saves the private and public key files to the default directory.
+    /// </summary>
+    /// <param name="bits">The key size in bits (must be 2048, 3072, or 4096). This value will never be null.</param>
+    /// <remarks>
+    /// If an unsupported key size is provided, the default size of 2048 bits is set.
+    /// The keys are written as Base64 encoded PEM files in the user's home directory.
+    /// </remarks>
     public static async Task GenerateKeys(int bits)
     {
         int[] keySizes = [2048, 3072, 4096];
