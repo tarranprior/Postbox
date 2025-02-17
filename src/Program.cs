ae1ce28a1941dd9f0579e733fbd514b842c60544
Tarran Prior <Tarranprior@protonmail.com>
2025-02-17 04:17:17 +0000

    📫 feat: add `generate-keys`, update `Program.cs`

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.CommandLine;
 using Serilog;
+using Postbox.KeyManagement;
 
 namespace Postbox;
 
@@ -14,11 +15,20 @@ class Program
 
         var root = new RootCommand("📫 Postbox is a lightweight command-line RSA encryption tool which allows users to generate key pairs, exchange public keys, encrypt and decrypt messages, and communicate securely over SMTP.");
 
-        var messageOption = new Option<string>(["-m", "--message"], "Specify a message.");
+        var bitsOption = new Option<int>(["-b", "--bits"], () => 2048, "Specify the number of bits for a key-pair. Default is 2048.");
+        var emailOption = new Option<string>(["-e", "--email"], "Specify an email address.");
         var fileOption = new Option<string>(["-f", "--file"], "Specify a file.");
         var keyOption = new Option<string>(["-k", "--key"], "Specify a public/private key.");
+        var messageOption = new Option<string>(["-m", "--message"], "Specify a message.");
         var outputOption = new Option<string>(["-o", "--output"], "Specify an output file.");
-        var emailOption = new Option<string>(["-e", "--email"], "Specify an email address.");
+        
+        var generateKeysCommand = new Command("generate-keys", "Generates a 2048-bit RSA key pair.")
+        {
+            bitsOption
+        };
+        generateKeysCommand.SetHandler(async (int bits) => await KeyManager.GenerateKeys(bits), bitsOption);
+
+        root.AddCommand(generateKeysCommand);
 
         await root.InvokeAsync(args);
     }
